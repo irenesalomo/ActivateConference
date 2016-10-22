b1109ddfba76c22c60c989dd61c3d6d9d5ec614e
Irene Salomo <irene.salomo@gmail.com>
2016-10-22 23:54:01 +1100

    WIP remove overlay text banner

--- wp-content/themes/hemingway/header.php
+++ wp-content/themes/hemingway/header.php
@@ -34,25 +34,9 @@
 						        </a>
 						        
 						    </div> <!-- /blog-logo -->
-					
-						<?php elseif ( get_bloginfo( 'description' ) || get_bloginfo( 'title' ) ) : ?>
-					
-							<div class="blog-info">
-							
-								<h2 class="blog-title">
-									<a href="<?php echo esc_url( home_url() ); ?>" title="<?php echo esc_attr( get_bloginfo( 'title' ) ); ?> &mdash; <?php echo esc_attr( get_bloginfo( 'description' ) ); ?>" rel="home"><?php echo esc_attr( get_bloginfo( 'title' ) ); ?></a>
-								</h2>
-								
-								<?php if ( get_bloginfo( 'description' ) ) { ?>
-								
-									<h3 class="blog-description"><?php echo esc_attr( get_bloginfo( 'description' ) ); ?></h3>
-									
-								<?php } ?>
-							
-							</div> <!-- /blog-info -->
-							
 						<?php endif; ?>
 									
+					</div> <!-- /header-inner -->
 					</div> <!-- /header-inner -->
 								
 				</div> <!-- /header -->
